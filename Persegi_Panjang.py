f6f95ee182216589fc68bd9c0a113eb971f3462e
Reynaldo Romero <105592890+Syunsuke23@users.noreply.github.com>
2022-10-09 16:26:57 +0800

    Add files via upload

---
+++ Persegi_Panjang.py
@@ -0,0 +1,9 @@
+print ("Masukkan Panjang : ")
+panjang = int (input ())
+print ("Masukkan lebar : ")
+lebar = int (input ())
+luas = panjang*lebar
+keliling = 2*(panjang*lebar)
+print ("Luas")
+print ("Luas Persegi Panjang adalah : " + str (luas))
+print ("keliling Persegi Panjang adalah : " + str (keliling))
