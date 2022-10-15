61c74a9b1b12dda745b138cfa073242d6de73a6b
Reynaldo Romero <105592890+Syunsuke23@users.noreply.github.com>
2022-10-15 18:07:06 +0800

    Create Kodingan M2 4.py

---
+++ Kodingan M2 4.py
@@ -0,0 +1,26 @@
+#1. Menghitung Debit 1.0
+
+print("masukkan nilai volume (satuan cm^3) : ")
+volume = float(input())
+print("Masukkan waktu (satuan detik) : ")
+waktu = float(input())
+debit = volume / waktu
+print("jadi hasil debitnya adalah " + str(debit))
+
+#2. Menghitung Debit 2.0
+
+print("masukkan nilai volume (satuan liter) : ")
+volume = float(input())
+print("Masukkan waktu (satuan detik) : ")
+waktu = float(input())
+debit = volume / waktu
+print("jadi hasil debitnya adalah " + str(debit))
+
+#3. Menghitung Volume 
+
+print("masukkan nilai volume (satuan liter) : ")
+volume = float(input())
+print("Masukkan waktu (satuan detik) : ")
+waktu = float(input())
+debit = volume / waktu
+print("jadi hasil debitnya adalah " + str(debit))
