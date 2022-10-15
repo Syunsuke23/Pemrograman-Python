186c9d4c7b9bde53e9390ee2a5293e43711c4906
Reynaldo Romero <105592890+Syunsuke23@users.noreply.github.com>
2022-10-15 17:15:39 +0800

    Create Kodingan M2 3.py

---
+++ Kodingan M2 3.py
@@ -0,0 +1,26 @@
+#1. Menentukan Kecepatan
+
+print("Masukkan Jarak : ")
+s = float(input())
+print("Masukkan waktu tempuh : ")
+t = float(input())
+kecepatan = s / t
+print("jadi kecepatannya adalah " + str(kecepatan))
+
+#2. Menentukan Jarak
+
+print("Masukkan waktu yang ditempuh : ")
+t = float(input())
+print("Masukkan kecepatan : ")
+v = float(input())
+s = t * v
+print("jadi jarak yang ditempuh adalah " + str(s))
+
+#3. Menentukan Waktu
+
+print("Masukkan kecepatan : ")
+v = float(input())
+print("Masukkan jarak yang ditempuh : ")
+s = float(input())
+t = s / v
+print("jadi waktu yang ditempuh adalah " + str(t))
